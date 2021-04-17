1ebcd813283da8d7aadfeb9bcc26a48a674f60cf
Amanuel Chukala <72323253+amanuel496@users.noreply.github.com>
2021-04-16 19:13:40 -0600

    Update server.js

--- server.js
+++ server.js
@@ -65,8 +65,8 @@ try {
 }
 mongoose.set('useCreateIndex', true);
 
-// const moviesRouter = require('./routes/movies');
-// const usersRouter = require('./routes/users');
+const moviesRouter = require('./routes/movies');
+const usersRouter = require('./routes/users');
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -75,8 +75,8 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-// app.use('/movies', moviesRouter);
-// app.use('/users', usersRouter);
+app.use('/movies', moviesRouter);
+app.use('/users', usersRouter);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
