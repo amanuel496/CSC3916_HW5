f61015ff8edcfbc45e09950c9720b591d7a408b2
Amanuel Chukala <72323253+amanuel496@users.noreply.github.com>
2021-04-16 18:54:09 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /*
-CSC3916 HW4
+CSC3916 HW3
 File: Server.js
 Description: Web API scaffolding for Movie API
  */
@@ -8,18 +8,16 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 //var authController = require('./auth');
-// var authJwtController = require('./auth_jwt');
+//var authJwtController = require('./auth_jwt');
 var jwt = require('jsonwebtoken');
 var cors = require('cors');
-var Review = require('./models/review');
+var User = require('./models/Users');
 
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 let Movie;
 Movie = require('./models/movie.model');
-
-
 
 var app = express();
 app.use(cors());
@@ -30,9 +28,31 @@ app.use(passport.initialize());
 
 var router = express.Router();
 
-
+// function getJSONObjectForMovieRequirement(req) {
+//     var json = {
+//         headers: "No headers",
+//         key: process.env.UNIQUE_KEY,
+//         body: "No body"
+//     };
+//
+//     if (req.body != null) {
+//         json.body = req.body;
+//     }
+//
+//     if (req.headers != null) {
+//         json.headers = req.headers;
+//     }
+//
+//     return json;
+// }
 
 const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+// );
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//     console.log("MongoDB database connection established successfully");
+// })
 
 mongoose.Promise = global.Promise;
 
@@ -47,25 +67,23 @@ mongoose.set('useCreateIndex', true);
 
 const moviesRouter = require('./routes/movies');
 const usersRouter = require('./routes/users');
-const reviewsRouter = require('./routes/reviews');
-//const path = require("path");
+const path = require("path");
 
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.use('/movies', moviesRouter);
 app.use('/users', usersRouter);
-app.use('/reviews', reviewsRouter);
 
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 
-// app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.listen(process.env.PORT || 8080);
 module.exports = app; // for testing only
 
